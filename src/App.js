5f98a2c99e1079bc4e8e2c259afdc79ce81e5b15
your_username <mithun.ka08@gmail.com>
2022-07-02 09:43:50 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import React, { Component } from "react"; //add this 1st
+import { CardList } from "./components/card-list/card-list.component";
+import { SearchBox } from "./components/search-box/search-box.component";
+import "./App.css";
+
+class App extends React.Component {
+  // 2nd step
+  constructor() {
+    super();
+    this.state = {
+      monsters: [],
+      searchField: "",
+    };
+  }
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) => this.setState({ monsters: users }));
+  }
+
+  handleChange = (e) => {
+    this.setState({ searchField: e.target.value });
+  };
+
+  render() {
+    const { monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter((monster) =>
+      monster.name.toLowerCase().includes(searchField.toLowerCase())
+    );
+    return (
+      <div className="App">
+        <h1>Monsters</h1>
+        <SearchBox
+          placeholder="search monsters"
+          handleChange={this.handleChange}
+        />
+        <CardList monsters={filteredMonsters} />
+      </div> // monsters is being passed as prop
+    );
+  }
+}
+
+export default App;
